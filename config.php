dd8bba6e436b67283c199f1c3cd895540e190d8c
Manuel Jesus <manueljesusmb@gmail.com>
2020-05-28 14:05:43 +0200

    Modificadas las constantes para recibir las variables desde Heroku

--- config.php
+++ config.php
@@ -1,14 +1,19 @@
 <?php
 require 'vendor/autoload.php';
 
-define('MAIL_HOST', 'smtp.gmail.com');
-define('MAIL_CHARSET', 'UTF-8');
-define('SMTP_SECURE', 'tls');
-define('USUARIO_CORREO', 'unCorreo');
-define('PASSWORD_CORREO', 'unaContraseña');
+define('MAIL_HOST', getenv('SERVIDOR_CORREO') ?: 'smtp.gmail.com');
+define('MAIL_CHARSET', getenv('CHARSET_CORREO') ?: 'UTF-8');
+define('SMTP_SECURE', getenv('SMTP_SEGURO') ?: 'tls');
+define('USUARIO_CORREO', getenv('USUARIO_CORREO') ?: 'unCorreo');
+define('PASSWORD_CORREO', getenv('PASSWORD_CORREO') ?: 'unaContraseña');
+
+define('SERVIDOR', getenv('SERVIDOR') ?: '127.0.0.1');
+define('USUARIO', getenv('USUARIO') ?: 'root');
+define('CLAVE', getenv('CLAVE') ?: '');
+define('NOMBRE_DB', getenv('NOMBRE_DB') ?: 'db_phpproject1');
 
 session_start();
-$db = new mysqli('127.0.0.1', 'root', '', 'db_phpproject1');
+$db = new mysqli(SERVIDOR, USUARIO, CLAVE, NOMBRE_DB);
 
 if (!$db) {
     exit('Fallo al conectarse a la base de datos');
